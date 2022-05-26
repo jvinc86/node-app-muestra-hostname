d968186526b4e00411a90be4e6da596314965ad2
Vincent <jvincent.camacho@gmail.com>
2022-05-26 13:53:14 +0200

    NewChanges

--- WebApp con NodeJS mas Nginx/App-NodeJS/server.js
+++ WebApp con NodeJS mas Nginx/App-NodeJS/server.js
@@ -13,5 +13,13 @@ app.get('/', (req, res) => {
     res.send(`VERSION 3: Hola Caracas, desde el servidor llamado ${el_hostname}`);
 });
 
+app.get('/miwebnginx', async (req, res) => {
+    const url = 'http://miwebnginx';
+    const response = await fetch(url);
+    const body = await response.text();
+    res.send(body);
+});
+
+
 app.listen(PORT, HOST);
 console.log(`🚀 Accede a la SUPER APP ingresando a 📲 http://localhost:${PORT}`);
